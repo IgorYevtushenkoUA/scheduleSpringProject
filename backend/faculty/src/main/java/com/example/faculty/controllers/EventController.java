8ef834efffbe34cdb08f397405240f823fdb0061
BeardimonR <beardimon2001@gmail.com>
2021-10-02 12:36:06 +0300

    remove duplicates

--- backend/faculty/src/main/java/com/example/faculty/controllers/EventController.java
+++
@@ -1,9 +0,0 @@
-package com.example.faculty.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/event")
-public class EventController {
-}
