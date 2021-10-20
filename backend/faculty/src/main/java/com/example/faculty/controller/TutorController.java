13645bd15cec17d45d26559989a11f50d90a9a08
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2021-10-20 18:42:05 +0300

    add tutor controller

--- backend/faculty/src/main/java/com/example/faculty/controller/TutorController.java
+++ backend/faculty/src/main/java/com/example/faculty/controller/TutorController.java
@@ -1,2 +1,33 @@
-package com.example.faculty.controller;public class TutorController {
+package com.example.faculty.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/tutor")
+public class TutorController {
+
+    @GetMapping(name = "/info")
+    public void tutorInfoForm() {
+    }
+
+    @PostMapping(name = "/into")
+    public void createTutorInfo() {
+    }
+
+    @GetMapping(name = "/schedule")
+    public void showTutorDefaultSchedule() {
+    }
+
+    @GetMapping(name = "/schedule/week/{num}")
+    public void showTutorScheduleForWeekN() {
+    }
+
+    @GetMapping(name = "event/{id}/update")
+    public void eventUpdateForm() {
+    }
+
+    @PostMapping(name = "event/{id}/update")
+    public void createRequestToUpdateEvent() {
+    }
+
 }
