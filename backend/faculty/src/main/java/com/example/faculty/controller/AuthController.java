e4cdde997999fb2e24c5f71e62d22bab1edf075c
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2021-12-04 12:17:27 +0200

    add register page

--- backend/faculty/src/main/java/com/example/faculty/controller/AuthController.java
+++ backend/faculty/src/main/java/com/example/faculty/controller/AuthController.java
@@ -15,10 +15,33 @@ public class AuthController {
     }
 
     @PostMapping("/signIn")
-    public String auth(Model model, @RequestParam(value = "email",defaultValue = "") String email) {
+    public String auth(Model model,
+                       @RequestParam(value = "email", defaultValue = "") String email,
+                       @RequestParam(value = "password", defaultValue = "") String password) {
         // todo write auth by email
         System.out.println("Email = " + email);
         return "redirect:/signIn";
     }
+
+    @GetMapping("/register")
+    public String register() {
+
+        return "registerStudent";
+    }
+
+    @PostMapping("/register")
+    public String registerAuth(Model model,
+                               @RequestParam("name") String name,
+                               @RequestParam("surname") String surname,
+                               @RequestParam("parental") String parental,
+                               @RequestParam("email") String email,
+                               @RequestParam("about") String about,
+                               @RequestParam("course") Integer course,
+                               @RequestParam("faculty") String faculty,
+                               @RequestParam("password") String password) {
+        System.out.println("");
+        return "redirect:/";
+    }
+
 
 }
